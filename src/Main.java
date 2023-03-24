aad99f1f354185d52f463f97dfaac31665398f02
KristinaKatarina <kristina.kaljumae@gmail.com>
2023-03-24 18:52:43 +0200

    Tuba muutmine abstaktseks klassiks

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        /*kiire näidis kampaania tööst, kui andmebaas on
-        valminud, siis tuleb paar tsüklit selle asemel teha
-         */
+
+        /*
+        Tuba asemel tuleb panna TubaKahele, TubaNeljale või VIP, sest Tuba on abstraktne
         Tuba tuba = new Tuba("1", false,false,true);
         Tuba tubaNr3 = new Tuba("3", true,false,true);
         Tuba vip = new Tuba("2",true,true,false);
@@ -10,6 +10,8 @@ public class Main {
         kampaania.lisaKampaaniasse(tuba);
         kampaania.lisaKampaaniasse(tubaNr3);
         kampaania.muudaVõitjaTuba();
+
+         */
 
     }
 }
--- src/Tuba.java
+++ src/Tuba.java
@@ -1,5 +1,38 @@
-public interface Tuba {
-    double öödeHind();
-    double lisaPakettideHind();
-    double hindKokku(); // Defineerime, et igas "Tuba kahele" ja "Tuba neljale" klassi peab eksisteerima meetod hindKokku.
+abstract class Tuba {
+    private String toaNumber;
+    private boolean kasVIP;
+    private boolean kasTubaOnKinni;
+    private boolean kasTubaKahele;
+
+    public Tuba(String toaNumber, boolean kasVIP, boolean kasTubaOnKinni, boolean kasTubaKahele) {
+        this.toaNumber = toaNumber;
+        this.kasVIP = kasVIP;
+        this.kasTubaOnKinni = kasTubaOnKinni;
+        this.kasTubaKahele = kasTubaKahele;
+    }
+
+    public String getToaNumber() {
+        return toaNumber;
+    }
+
+
+    public boolean isKasVIP() {
+        return kasVIP;
+    }
+
+
+
+    public boolean isKasTubaOnKinni() {
+        return kasTubaOnKinni;
+    }
+
+    public void setKasTubaOnKinni(boolean kasTubaOnKinni) {
+        this.kasTubaOnKinni = kasTubaOnKinni;
+    }
+
+
+
+    abstract double öödeHind();
+    abstract double lisaPakettideHind();
+    abstract double hindKokku(); // Defineerime, et igas "Tuba kahele" ja "Tuba neljale" klassi peab eksisteerima meetod hindKokku.
 }
